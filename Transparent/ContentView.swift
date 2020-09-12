5e6738a9d421e6148aab0fb389a5c30379acba64
Dmitry Kurkin <dmitry.kurkin@adviqo.com>
2020-09-13 01:10:52 +0200

    Initial Commit

---
+++ Transparent/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Transparent
+//
+//  Created by Dmitry Kurkin on 13.09.20.
+//  Copyright © 2020 Dmitry Kurkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
